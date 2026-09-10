e03354772e70d4787151f78aac788f979cb12a6b
Kimi <kimi@local>
2026-09-10 05:10:59 +0000

    Fix SDK: pakai IminPrinterLibrary V2.0.0.19 + API PrinterHelper resmi

--- android/app/src/main/java/com/imin/printapp/MainActivity.kt
+++ android/app/src/main/java/com/imin/printapp/MainActivity.kt
@@ -11,36 +11,38 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.imin.printersdk.Callback
-import com.imin.printersdk.IminPrintUtils
+import com.imin.printer.INeoPrinterCallback
+import com.imin.printer.PrinterHelper
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
-    private lateinit var utils: IminPrintUtils
+    private val helper = PrinterHelper.getInstance()
+
+    private val cb = object : INeoPrinterCallback() {
+        override fun onRunResult(isSuccess: Boolean) {}
+        override fun onReturnString(result: String?) {}
+        override fun onRaiseException(code: Int, msg: String?) {}
+        override fun onPrintResult(code: Int, msg: String?) {}
+    }
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Init printer built-in 80mm
-        utils = IminPrintUtils.getInstance(this)
-        utils.initPrinter(IminPrintUtils.PrintConnectType.USB)
+        helper.initPrinterService(this)
+        helper.initPrinter(packageName, cb)
 
         webView = findViewById(R.id.webView)
         webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true   // localStorage jalan
+        webView.settings.domStorageEnabled = true
         webView.webViewClient = WebViewClient()
-
-        // Jembatan JS: window.Android.printTicket(html)
         webView.addJavascriptInterface(PrinterBridge(), "Android")
 
         val prefs = getSharedPreferences("app", Context.MODE_PRIVATE)
         val savedUrl = prefs.getString("url", null)
-
         if (savedUrl != null) {
             findViewById<View>(R.id.setupPanel).visibility = View.GONE
             webView.loadUrl(savedUrl)
@@ -57,19 +59,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /** Render HTML jadi Bitmap 300dpi lebar 80mm lalu cetak TANPA dialog. */
     inner class PrinterBridge {
-
         @JavascriptInterface
         fun printTicket(html: String) {
             runOnUiThread {
                 val offscreen = WebView(this@MainActivity)
-                offscreen.settings.javaScriptEnabled = false
                 offscreen.loadDataWithBaseURL(null, html, "text/html", "UTF-8", null)
                 offscreen.webViewClient = object : WebViewClient() {
                     override fun onPageFinished(view: WebView, url: String?) {
                         view.post {
-                            val widthPx = 945 // ~300 dpi untuk 80mm
+                            val widthPx = 945 // 300dpi, lebar 80mm
                             view.measure(
                                 View.MeasureSpec.makeMeasureSpec(widthPx, View.MeasureSpec.EXACTLY),
                                 View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
@@ -78,16 +77,14 @@ class MainActivity : AppCompatActivity() {
                             val bmp = Bitmap.createBitmap(view.measuredWidth, view.measuredHeight, Bitmap.Config.ARGB_8888)
                             view.draw(Canvas(bmp))
 
-                            utils.printSingleBitmap(bmp, object : Callback {
-                                override fun onSuccess() {
-                                    utils.printAndFeedPaper(3)
-                                    utils.cutPaper()
+                            helper.printBitmap(bmp, object : INeoPrinterCallback() {
+                                override fun onRunResult(isSuccess: Boolean) {
+                                    helper.printAndFeedPaper(3)
+                                    helper.fullCutAndFeedPaper(5)
                                 }
-                                override fun onError(code: Int, msg: String?) {
-                                    runOnUiThread {
-                                        Toast.makeText(this@MainActivity, "Print error: $msg", Toast.LENGTH_SHORT).show()
-                                    }
-                                }
+                                override fun onReturnString(result: String?) {}
+                                override fun onRaiseException(code: Int, msg: String?) {}
+                                override fun onPrintResult(code: Int, msg: String?) {}
                             })
                         }
                     }
